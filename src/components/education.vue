73b28c22c141b2ddb64aa3088de97c64c2c8559b
Rodion Gavrilov <rodiongavrilov@gmail.com>
2022-02-18 00:36:54 +0300

    [add] personal info and education markup

---
+++ src/components/education.vue
@@ -0,0 +1,62 @@
+<template>
+  <form action="">
+    <div class="form-item">
+      <label>Образование</label>
+      <v-select v-model="value" :options="options" />
+      <!--      <select v-model="selected">-->
+      <!--        <option v-for="option in options" :key="option.value">-->
+      <!--          {{ option.text }}-->
+      <!--        </option>-->
+      <!--      </select>-->
+    </div>
+    <div class="form-item">
+      <label>Навыки</label>
+      <v-select
+        taggable
+        multiple
+        :reduce="(option) => option.id"
+        :options="options2"
+      />
+    </div>
+    <div class="form-item sms-notification">
+      <label class="container">
+        <input type="checkbox" checked="checked" />
+        <span class="checkmark"></span>
+        Оповестить по SMS
+      </label>
+    </div>
+    <div class="form-item buttons">
+      <button @click.prevent="$router.push({ name: 'home' })">Назад</button>
+      <button>Вперед</button>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  name: "educationData",
+  data() {
+    return {
+      value: [],
+      value2: [],
+      selected: 1,
+      options: [
+        "Среднее",
+        "Средне-специальное",
+        "Неоконченное высшее",
+        "Бакалавр",
+        "Магистрант",
+        "Аспирант",
+      ],
+      options2: [
+        { label: "Vue", id: 1 },
+        { label: "React", id: 2 },
+        { label: "Angular", id: 3 },
+        { label: "Svelte", id: 4 },
+        { label: "Ember.js", id: 5 },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./styles/style.scss";
+import "vue-select/dist/vue-select.css";
+import VCalendar from "v-calendar";
+import vSelect from "vue-select";
 
 Vue.config.productionTip = false;
-
+Vue.use(VCalendar);
+Vue.component("v-select", vSelect);
 new Vue({
   router,
   store,
